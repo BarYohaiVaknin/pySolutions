a5b8e7d86cbe0e4e4e578704eb3f9fa63fce863d
Bar Yohai Vaknin <baryvee@gmail.com>
2023-01-09 11:58:37 +0200

    complete solution for minimumIndexSumOf2Lists.py

--- leetCode/minimumIndexSumOf2Lists.py
+++ leetCode/minimumIndexSumOf2Lists.py
@@ -1,20 +1,17 @@
-list1 = ["Shogun","Tapioca Express","Burger King","KFC"]
+list1 = ["Shogun","Piatti","Tapioca Express","Burger King","KFC"]
 list2 = ["Piatti","The Grill at Torrey Pines","Hungry Hunter Steakhouse","Shogun"]
 results = []
 def main (list1,list2):
-    minCounter = len(list1) + len(list2)
-    if len(list1) >= len(list2):
-        for item in list1:
-            if item in list2 and (list1.index(item) + list2.index(item) <= minCounter):
-                minCounter = list1.index(item) + list2.index(item)
+    minIndex = len(list1) + len(list2)
+    for item in list1:
+        if item in list2 and (list1.index(item) + list2.index(item) <= minIndex):
+            if list1.index(item) + list2.index(item) < minIndex:
+                minIndex = list1.index(item) + list2.index(item)
+                results.clear()
                 results.append(item)
-        return results
-    else:
-        for item in list2:
-                if item in list1:
-                    minCounter = list1.index(item) + list2.index(item)
-                    results.append(item)
-        return results
+            else:
+                results.append(item)
+    return results
 print(main(list1,list2))
 
 
